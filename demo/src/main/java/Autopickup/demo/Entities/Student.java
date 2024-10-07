2e7764f5f3bc0f3defb71d4117c07cd2a289099a
karti <kartikkumarji24@gmail.com>
2024-10-07 20:52:29 +0530

    my first commit

---
+++ demo/src/main/java/Autopickup/demo/Entities/Student.java
@@ -0,0 +1,11 @@
+package Autopickup.demo.Entities;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private String name;
+    private int age;
+    private int classes;
+    private String stream;
+}
---
+++ demo/src/test/java/Autopickup/demo/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package Autopickup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
